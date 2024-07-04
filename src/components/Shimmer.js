3e7aa9c2d6c7373b97ecc2319cb5a1149b5ce1a8
p3g-18 <parthgoswami30@gmail.com>
2024-07-04 16:01:59 +0530

    shimmer effect optimized

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,72 +1,21 @@
 const Shimmer = () => {
+  const shimmerCards = Array.from({ length: 10 });
+
   return (
-    <>
-      <div className="shim-container flex flex-wrap m-[100px] justify-between  ">
-        <div className="shim-cards m-[16px] h-[375px] w-[250px]  rounded-2xl  bg-slate-200 animate-pulse">
-          <div className="w-[100%] h-[250px] rounded-2xl bg-slate-300 "></div>
+    <div className="shim-container flex flex-wrap m-[100px] justify-between">
+      {shimmerCards.map((_, index) => (
+        <div
+          key={index}
+          className="shim-cards m-[16px] h-[375px] w-[250px] rounded-2xl bg-slate-200 animate-pulse"
+        >
+          <div className="w-[100%] h-[250px] rounded-2xl bg-slate-300"></div>
           <div className="w-[50%] h-[25px] mt-4 bg-slate-300 ml-4 rounded-sm"></div>
-          <div className="w-[80%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[60%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[40%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
+          <div className="w-[80%] h-[15px] bg-slate-300 mt-2 rounded-sm ml-4"></div>
+          <div className="w-[60%] h-[15px] bg-slate-300 mt-2 rounded-sm ml-4"></div>
+          <div className="w-[40%] h-[15px] bg-slate-300 mt-2 rounded-sm ml-4"></div>
         </div>
-        <div className="shim-cards m-[16px] h-[375px] w-[250px]  rounded-2xl  bg-slate-200 animate-pulse">
-          <div className="w-[100%] h-[250px] rounded-2xl bg-slate-300 "></div>
-          <div className="w-[50%] h-[25px] mt-4 bg-slate-300 ml-4 rounded-sm"></div>
-          <div className="w-[80%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[60%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[40%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-        </div>
-        <div className="shim-cards m-[16px] h-[375px] w-[250px]  rounded-2xl  bg-slate-200 animate-pulse">
-          <div className="w-[100%] h-[250px] rounded-2xl bg-slate-300 "></div>
-          <div className="w-[50%] h-[25px] mt-4 bg-slate-300 ml-4 rounded-sm"></div>
-          <div className="w-[80%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[60%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[40%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-        </div>
-        <div className="shim-cards m-[16px] h-[375px] w-[250px]  rounded-2xl  bg-slate-200 animate-pulse">
-          <div className="w-[100%] h-[250px] rounded-2xl bg-slate-300 "></div>
-          <div className="w-[50%] h-[25px] mt-4 bg-slate-300 ml-4 rounded-sm"></div>
-          <div className="w-[80%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[60%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[40%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-        </div>
-        <div className="shim-cards m-[16px] h-[375px] w-[250px]  rounded-2xl  bg-slate-200 animate-pulse">
-          <div className="w-[100%] h-[250px] rounded-2xl bg-slate-300 "></div>
-          <div className="w-[50%] h-[25px] mt-4 bg-slate-300 ml-4 rounded-sm"></div>
-          <div className="w-[80%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[60%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[40%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-        </div>
-        <div className="shim-cards m-[16px] h-[375px] w-[250px]  rounded-2xl  bg-slate-200 animate-pulse">
-          <div className="w-[100%] h-[250px] rounded-2xl bg-slate-300 "></div>
-          <div className="w-[50%] h-[25px] mt-4 bg-slate-300 ml-4 rounded-sm"></div>
-          <div className="w-[80%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[60%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[40%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-        </div>
-        <div className="shim-cards m-[16px] h-[375px] w-[250px]  rounded-2xl  bg-slate-200 animate-pulse">
-          <div className="w-[100%] h-[250px] rounded-2xl bg-slate-300 "></div>
-          <div className="w-[50%] h-[25px] mt-4 bg-slate-300 ml-4 rounded-sm"></div>
-          <div className="w-[80%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[60%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[40%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-        </div>
-        <div className="shim-cards m-[16px] h-[375px] w-[250px]  rounded-2xl  bg-slate-200 animate-pulse">
-          <div className="w-[100%] h-[250px] rounded-2xl bg-slate-300 "></div>
-          <div className="w-[50%] h-[25px] mt-4 bg-slate-300 ml-4 rounded-sm"></div>
-          <div className="w-[80%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[60%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[40%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-        </div>
-        <div className="shim-cards m-[16px] h-[375px] w-[250px]  rounded-2xl  bg-slate-200 animate-pulse">
-          <div className="w-[100%] h-[250px] rounded-2xl bg-slate-300 "></div>
-          <div className="w-[50%] h-[25px] mt-4 bg-slate-300 ml-4 rounded-sm"></div>
-          <div className="w-[80%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[60%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-          <div className="w-[40%] h-[15px]  bg-slate-300 mt-2 rounded-sm ml-4"></div>
-        </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 };
 
